67d3fb7317efc44d1c6355052d2146019ed8e76e
super1-1star <agsuperstar1142@gmail.com>
2022-01-06 22:19:30 +0530

    structured

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className='container'>
+        <div className='left-col blue'>
+          <div className='tool-box blue'></div>
+          <div className='input-editor red'></div>
+        </div>
+        <div className='right-col'>
+          <div className='performance-container red'></div>
+          <div className='add-ons-container yellow'></div>
+          <div className='keywords-to-use-container blue'></div>
+        </div>
+      </div>
+    </main>
   );
 }
 
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import { render } from "@testing-library/react";
+import react from "react";
+
+function Main(){
+    
+    return(
+        <main>
+            <div className="left-col">
+                <div className="tool-box">
+
+                </div>
+                <div className="input-editor"></div>   
+            </div>
+            <div className="right-col"></div>
+        </main>
+    )
+}
+
+
+export default Main;
